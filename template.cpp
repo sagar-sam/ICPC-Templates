a338607e5ea0a6aa173d41879aac096d711874ed
sagar <sagarkhatri227.sk@gmail.com>
2017-11-04 11:22:40 +0530

    first commit

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define mod 1000000007ll
+#define inf 1000000000
+#define for(i,a,b) for(int i=(a);i<(b);i++)
+#define fs first
+#define sc second
+#define pb push_back
+#define mp make_pair
+#define ins insert
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef string st;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<st> vs;
+
+typedef map<int,int> mii;
+typedef map<st,int> msi;
+typedef map<char,int> mci;
+
+typedef set<int> si;
+typedef set<st> ss;
+
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+const double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+
+	}
+}
